784af768c9f6ea487865c5cd05e328b987441208
Haokun Liu <haokunliu412@gmail.com>
2019-11-19 14:03:16 -0500

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,29 @@
-import logging
+import logging as log
+import torch
 
 EPSILON = 1e-8
 
 
 def config_logging(log_file):
-    logging.basicConfig(
-        level=logging.INFO,
+    log.basicConfig(
+        level=log.INFO,
         format="%(asctime)s [%(threadName)-12.12s] %(message)s",
-        handlers=[logging.FileHandler(log_file), logging.StreamHandler()],
+        handlers=[log.FileHandler(log_file), log.StreamHandler()],
     )
+
+
+def load_model(load_ckpt, model):
+    """
+    Load a model, for training, evaluation or prediction
+    """
+    model_state = torch.load(load_ckpt)
+    model.load_state_dict(model_state)
+    log.info("Load parameters from %s" % load_ckpt)
+
+
+def save_model(save_ckpt, model):
+    """
+    Save the parameters of the model to a checkpoint
+    """
+    torch.save(model.state_dict(), save_ckpt)
+    log.info("Save parameters for %s" % save_ckpt)
