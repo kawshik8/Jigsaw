d7c67b07c61bdbdd7bf44b94e1613142ab7406d8
kawshik <kawshik98@gmail.com>
2019-12-09 22:15:20 -0500

    Update models.py

--- src/models.py
+++ src/models.py
@@ -54,26 +54,6 @@ def flatten_dim(layer):
     else:
         return 8192
 
-class nce_loss(Module):
-    def __init__(self, size_average = True):
-        super(nce_loss, self).__init__()
-        self.size_average = size_average
-
-    def forward(self, input):
-        logits, nce_target = input
-
-        N, Kp1 = logits.size()  # num true x (num_noise+1)
-
-        loss = F.binary_cross_entropy_with_logits(logits, nce_target, reduce = False)
-
-        loss = torch.sum(loss)
-
-        if self.size_average:
-            loss /= N
-
-        return loss
-
-
 class JigsawModel(nn.Module):
     def __init__(self, args):
         super().__init__()
